86f567d23b6f0ad670d25f1bbc5fd74aaed50d0d
harryscholes <harryscholes@users.noreply.github.com>
2025-11-30 20:00:19 +0200

    multiple queries

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,6 @@ use clap::Parser;
 use futures_util::{Stream, StreamExt, stream};
 use rand::{Rng, rng};
 use tokio::sync::Mutex;
-use tqdm::pbar;
 use uuid::Uuid;
 use vortex::{
     ArrayRef, ArraySession, IntoArray, ToCanonical,
@@ -24,7 +23,7 @@ use vortex::{
     dtype::{DType, Nullability, StructFields},
     encodings::sequence::SequenceArray,
     error::VortexResult,
-    expr::{and_collect, col, eq, lit, lt, root, select},
+    expr::{and_collect, col, eq, lit, lt, or_collect, root, select},
     file::{OpenOptionsSessionExt, WriteOptionsSessionExt},
     io::session::RuntimeSession,
     layout::session::LayoutSession,
@@ -52,22 +51,24 @@ struct Opt {
     rows: usize,
     #[arg(long, short = 'd', default_value_t = 1024)]
     dimension: usize,
-    #[arg(long, short = 'k', default_value_t = 10)]
-    top_k: usize,
     #[arg(long, short = 'c', default_value_t = 1024)]
     chunk_size: usize,
+    #[arg(long, short = 'k', default_value_t = 10)]
+    top_k: usize,
+    #[arg(long, short = 'q', default_value_t = 100)]
+    queries: usize,
     #[arg(long)]
     include_values: bool,
     #[arg(long)]
     include_metadata: bool,
     #[arg(long)]
     progress: bool,
-    #[arg(long, default_value_t = 5)]
-    rand_categorical_cardinality: u32,
     #[arg(long)]
-    rand_categorical: Option<u32>,
+    rand_categorical_cardinality: Option<u32>,
     #[arg(long, default_value_t = 0.5)]
     rand_float_selectivity: f64,
+    #[arg(long)]
+    print_results: bool,
 }
 
 #[tokio::main]
@@ -80,12 +81,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         chunk_size,
         dimension,
         top_k,
+        queries,
         include_values,
         include_metadata,
         progress,
         rand_categorical_cardinality,
-        rand_categorical,
         rand_float_selectivity,
+        print_results,
     } = opt;
 
     let session = VortexSession::empty()
@@ -96,13 +98,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     vortex::file::register_default_encodings(&session);
 
-    println!("writing vortex file to {path:?}");
     let write_stage_start = Instant::now();
 
     let ivf_partitions = rows.isqrt();
     let ivf_partition_size = (rows + ivf_partitions - 1) / ivf_partitions;
 
-    let pbar = progress.then(|| Arc::new(Mutex::new(pbar(Some(rows)))));
+    let rand_categorical_cardinality =
+        rand_categorical_cardinality.unwrap_or_else(|| (rows.isqrt() as u32 / 10).max(3));
+
+    let pbar = progress.then(|| Arc::new(Mutex::new(tqdm::pbar(Some(rows)))));
 
     let chunk_stream = stream::try_unfold((0usize, pbar), move |(rows_written, pbar)| async move {
         if rows_written >= rows {
@@ -146,13 +150,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             PrimitiveArray::from_iter((0..chunk_size).map(|_| rng().random_range(0.0f64..1.0)));
 
         let rand_categorical_1 = PrimitiveArray::from_iter(
-            (0..chunk_size).map(|_| rng().random_range(0u32..rand_categorical_cardinality)),
+            (0..chunk_size).map(|_| rng().random_range(0..rand_categorical_cardinality)),
         );
         let compressor = BtrBlocksCompressor::default();
         let rand_categorical_1 = compressor.compress(rand_categorical_1.as_ref())?;
 
         let rand_categorical_2 = PrimitiveArray::from_iter(
-            (0..chunk_size).map(|_| rng().random_range(0u32..rand_categorical_cardinality)),
+            (0..chunk_size).map(|_| rng().random_range(0..rand_categorical_cardinality)),
         );
         let compressor = BtrBlocksCompressor::default();
         let rand_categorical_2 = compressor.compress(rand_categorical_2.as_ref())?;
@@ -229,184 +233,202 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .write(&mut file, array_stream)
         .await?;
 
+    let file_size = write_summary.size();
+    println!("file size: {:.2} MB", file_size as f64 / (1 << 20) as f64);
+
     println!(
         "write stage elapsed time: {:?}",
         write_stage_start.elapsed()
     );
 
-    let file_size = write_summary.size();
-    println!("file size: {:.2} MB", file_size as f64 / (1 << 20) as f64);
-
-    println!("reading vortex file from {path:?}");
     let read_stage_start = Instant::now();
 
     let file = session.open_options().open(path).await.unwrap();
 
     let metrics = file.metrics();
 
-    // Mock query
-    let query_projection = BoolArray::from_iter((0..opt.dimension).map(|_| rng().random_bool(0.5)));
-    let max_ivf_partition_idx = rows / ivf_partition_size;
-    let query_ivf_partition_idx = rng().random_range(0..max_ivf_partition_idx);
-    let query_rand_categorical_1 =
-        rand_categorical.unwrap_or_else(|| rng().random_range(0u32..rand_categorical_cardinality));
-    let query_rand_categorical_2 =
-        rand_categorical.unwrap_or_else(|| rng().random_range(0u32..rand_categorical_cardinality));
+    let pbar = progress.then(|| Arc::new(Mutex::new(tqdm::pbar(Some(queries)))));
 
-    let stream = file
-        .scan()?
-        .with_filter(
-            and_collect(vec![
-                eq(
-                    col(IVF_PARTITION_IDX_COL),
-                    lit(query_ivf_partition_idx as u32),
-                ),
-                eq(col(RAND_CATEGORICAL_1_COL), lit(query_rand_categorical_1)),
-                eq(col(RAND_CATEGORICAL_2_COL), lit(query_rand_categorical_2)),
-                lt(col(RAND_FLOAT_COL), lit(rand_float_selectivity)),
-            ])
-            .unwrap(),
-        )
-        .with_projection(select([ROW_IDX_COL, ID_COL, PROJECTION_COL], root()))
-        .into_array_stream()?;
+    for _ in 0..queries {
+        let query_start = Instant::now();
 
-    let mut stream = Box::pin(stream);
+        let query_projection =
+            BoolArray::from_iter((0..opt.dimension).map(|_| rng().random_bool(0.5)));
+        let nprobe = ivf_partitions.isqrt();
+        let query_ivf_partition_idxs = (0..nprobe).map(|_| rng().random_range(0..ivf_partitions));
+        let query_rand_categorical_1 = rng().random_range(0..rand_categorical_cardinality);
+        let query_rand_categorical_2 = rng().random_range(0..rand_categorical_cardinality);
 
-    let mut heap = BinaryHeap::<HeapElement>::new();
+        let stream = file
+            .scan()?
+            .with_filter(
+                and_collect(vec![
+                    or_collect(
+                        query_ivf_partition_idxs
+                            .map(|idx| eq(col(IVF_PARTITION_IDX_COL), lit(idx as u32))),
+                    )
+                    .unwrap(),
+                    eq(col(RAND_CATEGORICAL_1_COL), lit(query_rand_categorical_1)),
+                    eq(col(RAND_CATEGORICAL_2_COL), lit(query_rand_categorical_2)),
+                    lt(col(RAND_FLOAT_COL), lit(rand_float_selectivity)),
+                ])
+                .unwrap(),
+            )
+            .with_projection(select([ROW_IDX_COL, ID_COL, PROJECTION_COL], root()))
+            .into_array_stream()?;
 
-    while let Some(array) = stream.next().await {
-        let array = array?;
+        let mut stream = Box::pin(stream);
 
-        let s = array.to_struct();
-        let row_idxs = s.field_by_name(ROW_IDX_COL)?.to_primitive();
-        let ids = s.field_by_name(ID_COL)?.to_varbinview();
-        let projections = s.field_by_name(PROJECTION_COL)?.to_fixed_size_list();
+        let mut heap = BinaryHeap::<HeapElement>::new();
 
-        for i in 0..s.len() {
-            let row_idx = row_idxs.scalar_at(i);
-            let row_idx = row_idx.as_primitive().typed_value().unwrap();
+        while let Some(array) = stream.next().await {
+            let array = array?;
 
-            let id = ids.scalar_at(i);
-            let id = id.as_utf8().value().unwrap();
-            let id = id.as_str();
+            let s = array.to_struct();
+            let row_idxs = s.field_by_name(ROW_IDX_COL)?.to_primitive();
+            let ids = s.field_by_name(ID_COL)?.to_varbinview();
+            let projections = s.field_by_name(PROJECTION_COL)?.to_fixed_size_list();
 
-            let projection_array = projections.fixed_size_list_elements_at(i);
-            let projection = projection_array.to_bool();
+            for i in 0..s.len() {
+                let row_idx = row_idxs.scalar_at(i);
+                let row_idx = row_idx.as_primitive().typed_value().unwrap();
 
-            let distance = compare(
-                query_projection.as_ref(),
-                projection.as_ref(),
-                Operator::NotEq,
-            )?
-            .to_bool()
-            .as_bool_typed()
-            .true_count()?;
+                let id = ids.scalar_at(i);
+                let id = id.as_utf8().value().unwrap();
+                let id = id.as_str();
 
-            if heap.len() < top_k {
-                heap.push(HeapElement {
-                    row_idx,
-                    id: id.to_string(),
-                    distance,
-                });
-            } else {
-                if let Some(min) = heap.peek() {
-                    if distance < min.distance {
-                        heap.pop();
-                        heap.push(HeapElement {
-                            row_idx,
-                            id: id.to_string(),
-                            distance,
-                        });
+                let projection_array = projections.fixed_size_list_elements_at(i);
+                let projection = projection_array.to_bool();
+
+                let distance = compare(
+                    query_projection.as_ref(),
+                    projection.as_ref(),
+                    Operator::NotEq,
+                )?
+                .to_bool()
+                .as_bool_typed()
+                .true_count()?;
+
+                if heap.len() < top_k {
+                    heap.push(HeapElement {
+                        row_idx,
+                        id: id.to_string(),
+                        distance,
+                    });
+                } else {
+                    if let Some(min) = heap.peek() {
+                        if distance < min.distance {
+                            heap.pop();
+                            heap.push(HeapElement {
+                                row_idx,
+                                id: id.to_string(),
+                                distance,
+                            });
+                        }
                     }
                 }
             }
         }
-    }
 
-    let top_k = heap.into_sorted_vec();
+        let top_k = heap.into_sorted_vec();
 
-    let id_to_distance = top_k
-        .iter()
-        .map(|h| (h.id.clone(), h.distance))
-        .collect::<HashMap<_, _>>();
+        let id_to_distance = top_k
+            .iter()
+            .map(|h| (h.id.clone(), h.distance))
+            .collect::<HashMap<_, _>>();
 
-    let mut row_idxs = top_k.iter().map(|h| h.row_idx).collect::<Vec<_>>();
-    row_idxs.sort();
-    let selection = Selection::IncludeByIndex(Buffer::from_iter(row_idxs));
+        let mut row_idxs = top_k.iter().map(|h| h.row_idx).collect::<Vec<_>>();
+        row_idxs.sort();
+        let selection = Selection::IncludeByIndex(Buffer::from_iter(row_idxs));
 
-    let mut projection_mask = vec![ID_COL];
-    if include_values {
-        projection_mask.push(VECTOR_COL);
-    }
-    if include_metadata {
-        projection_mask.push(RAND_FLOAT_COL);
-        projection_mask.push(RAND_CATEGORICAL_1_COL);
-        projection_mask.push(RAND_CATEGORICAL_2_COL);
-    }
+        let mut projection_mask = vec![ID_COL];
+        if include_values {
+            projection_mask.push(VECTOR_COL);
+        }
+        if include_metadata {
+            projection_mask.push(RAND_FLOAT_COL);
+            projection_mask.push(RAND_CATEGORICAL_1_COL);
+            projection_mask.push(RAND_CATEGORICAL_2_COL);
+        }
 
-    let results = file
-        .scan()?
-        .with_selection(selection)
-        .with_projection(select(projection_mask.as_slice(), root()))
-        .into_array_stream()?
-        .read_all()
-        .await?;
+        let results = file
+            .scan()?
+            .with_selection(selection)
+            .with_projection(select(projection_mask.as_slice(), root()))
+            .into_array_stream()?
+            .read_all()
+            .await?;
 
-    let s = results.to_struct();
-    let ids = s.field_by_name(ID_COL)?.to_varbinview();
-    let vectors = s.field_by_name(VECTOR_COL);
-    let rand_floats = s.field_by_name(RAND_FLOAT_COL);
-    let rand_categorical_1 = s.field_by_name(RAND_CATEGORICAL_1_COL);
-    let rand_categorical_2 = s.field_by_name(RAND_CATEGORICAL_2_COL);
+        let s = results.to_struct();
+        let ids = s.field_by_name(ID_COL)?.to_varbinview();
+        let vectors = s.field_by_name(VECTOR_COL);
+        let rand_floats = s.field_by_name(RAND_FLOAT_COL);
+        let rand_categorical_1 = s.field_by_name(RAND_CATEGORICAL_1_COL);
+        let rand_categorical_2 = s.field_by_name(RAND_CATEGORICAL_2_COL);
 
-    let mut results = (0..s.len())
-        .map(|i| {
-            let id_scalar = ids.scalar_at(i);
-            let id_utf8_value = id_scalar.as_utf8().value().unwrap();
-            let id = id_utf8_value.as_str().to_string();
+        let mut results = (0..s.len())
+            .map(|i| {
+                let id_scalar = ids.scalar_at(i);
+                let id_utf8_value = id_scalar.as_utf8().value().unwrap();
+                let id = id_utf8_value.as_str().to_string();
 
-            let distance = *id_to_distance.get(&id).unwrap();
+                let distance = *id_to_distance.get(&id).unwrap();
 
-            let vector = include_values.then(|| {
-                let vectors = vectors.as_ref().unwrap().to_fixed_size_list();
-                vectors.fixed_size_list_elements_at(i).to_primitive()
-            });
+                let vector = include_values.then(|| {
+                    let vectors = vectors.as_ref().unwrap().to_fixed_size_list();
+                    vectors.fixed_size_list_elements_at(i).to_primitive()
+                });
 
-            let metadata = include_metadata.then(|| {
-                let rand_floats = rand_floats.as_ref().unwrap().to_primitive();
-                let rand_float = rand_floats
-                    .scalar_at(i)
-                    .as_primitive()
-                    .typed_value()
-                    .unwrap();
-                let rand_categorical_1 = rand_categorical_1.as_ref().unwrap().to_primitive();
-                let rand_categorical_1 = rand_categorical_1
-                    .scalar_at(i)
-                    .as_primitive()
-                    .typed_value()
-                    .unwrap();
-                let rand_categorical_2 = rand_categorical_2.as_ref().unwrap().to_primitive();
-                let rand_categorical_2 = rand_categorical_2
-                    .scalar_at(i)
-                    .as_primitive()
-                    .typed_value()
-                    .unwrap();
-                (rand_float, rand_categorical_1, rand_categorical_2)
-            });
+                let metadata = include_metadata.then(|| {
+                    let rand_floats = rand_floats.as_ref().unwrap().to_primitive();
+                    let rand_float = rand_floats
+                        .scalar_at(i)
+                        .as_primitive()
+                        .typed_value()
+                        .unwrap();
+                    let rand_categorical_1 = rand_categorical_1.as_ref().unwrap().to_primitive();
+                    let rand_categorical_1 = rand_categorical_1
+                        .scalar_at(i)
+                        .as_primitive()
+                        .typed_value()
+                        .unwrap();
+                    let rand_categorical_2 = rand_categorical_2.as_ref().unwrap().to_primitive();
+                    let rand_categorical_2 = rand_categorical_2
+                        .scalar_at(i)
+                        .as_primitive()
+                        .typed_value()
+                        .unwrap();
+                    (rand_float, rand_categorical_1, rand_categorical_2)
+                });
 
-            ResultElement {
-                id,
-                distance,
-                vector,
-                metadata,
+                ResultElement {
+                    id,
+                    distance,
+                    vector,
+                    metadata,
+                }
+            })
+            .collect::<Vec<_>>();
+
+        results.sort_by_key(|r| r.distance);
+
+        if print_results {
+            for result in results {
+                println!("{}", result);
             }
-        })
-        .collect::<Vec<_>>();
+        }
 
-    results.sort_by_key(|r| r.distance);
+        metrics
+            .histogram("query.duration")
+            .update(query_start.elapsed().as_nanos() as i64);
 
-    for result in results {
-        println!("{}", result);
+        if let Some(pbar) = &pbar {
+            _ = pbar.lock().await.update(1);
+        }
+    }
+
+    if let Some(pbar) = &pbar {
+        _ = pbar.lock().await.close();
     }
 
     println!("read stage elapsed time: {:?}", read_stage_start.elapsed());
@@ -424,14 +446,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             Metric::Histogram(hist) => {
                 let snapshot = hist.snapshot();
                 let p50 = snapshot.value(0.5);
+                let p90 = snapshot.value(0.9);
                 let p99 = snapshot.value(0.99);
-                println!("histogram {name}: p50={p50}, p99={p99}");
+                println!("histogram {name}: p50={p50}, p90={p90}, p99={p99}");
             }
             Metric::Timer(timer) => {
                 let snapshot = timer.snapshot();
                 let p50 = snapshot.value(0.5);
+                let p90 = snapshot.value(0.9);
                 let p99 = snapshot.value(0.99);
-                println!("timer {name}: p50={p50}, p99={p99}");
+                println!("timer {name}: p50={p50}, p90={p90}, p99={p99}");
             }
             _ => {}
         }
